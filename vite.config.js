5885a432fd2721a94c0caf313c20fc23a4e6de46
Barathraj-M <barathrajbrix25@gmail.com>
2025-11-04 13:04:48 +0530

    check - 1

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { URL } from 'whatwg-url';
+global.URL = URL;
 
 // https://vite.dev/config/
 export default defineConfig({
